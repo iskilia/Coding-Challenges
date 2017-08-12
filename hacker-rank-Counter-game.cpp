5cfffae747d53574aedb13ded3211c1e1219bc65
Isaac Kilis <isaackilis@gmail.com>
2017-08-12 13:37:56 -0500

    ccounter game

---
+++ hacker-rank-Counter-game.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int numTestcases;
+    cin >> numTestcases;
+    for (int i = 0; i < numTestcases; i++){
+        unsigned long long n;
+        int count = 0;
+        cin >> n;
+        n--;
+        while(n) {
+            n &= (n-1);
+            count++;
+        }
+        cout << (count & 1 ? "Louise" : "Richard") << endl;
+    }
+    return 0;
+}
+// Richard wins print "Richard"
+// Louise wins print "Louise"
+// Louise go first
