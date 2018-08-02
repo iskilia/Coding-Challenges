23cc3d2d9e7651bc796238692e625fbae154177f
Isaac Kilis <isaackilis@gmail.com>
2018-08-02 01:40:25 -0500

    timetowords

---
+++ Algorithms:ProblemSolving/hacker-rank-time-in-words.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> numbers = {
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty",
+        "twenty one",
+        "twenty two",
+        "twenty three",
+        "twenty four",
+        "twenty five",
+        "twenty six",
+        "twenty seven",
+        "twenty eight",
+        "twenty nine"
+    };
+
+// Complete the timeInWords function below.
+string timeInWords(int h, int m) {
+  if(m == 0) return numbers[h]+ " o'clock";
+  else if( m >= 1 && m <= 30) return numbers[m] + " minutes to " + numbers[h];
+  else return numbers[m] + " minutes past " + numbers[h];
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int h;
+    cin >> h;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int m;
+    cin >> m;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string result = timeInWords(h, m);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
