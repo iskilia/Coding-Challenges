1e5e99588c666b2b323cbfe3def180dcc2a5f744
Isaac Kilis <isaackilis@gmail.com>
2018-10-21 15:29:00 -0500

    more hackerrank challenges

---
+++ Algorithms:ProblemSolving/hacker-rank-tutorial-intro.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int binarySearch(int V, vector<int> arr, int left, int right){
+    if(right >= left){
+        int mid = (left + right)/2;
+        if(arr[mid] == V) return mid;
+        else if(arr[mid] > V) return binarySearch(V, arr, left, mid-1);
+        else return binarySearch(V, arr, mid+1, right);
+    }
+    return -1;
+}
+// Complete the introTutorial function below.
+int introTutorial(int V, vector<int> arr) {
+    return binarySearch(V, arr, 0, arr.size()-1);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int V;
+    cin >> V;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = introTutorial(V, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
