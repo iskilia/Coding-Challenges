ec19b4cea015229b3d858c56cb188b2fa77dae04
Isaac Kilis <isaackilis@gmail.com>
2018-10-26 19:51:25 -0500

    caesar cipher

---
+++ Algorithms:ProblemSolving/hacker-rank-caesar-cipher.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the caesarCipher function below.
+string caesarCipher(string s, int k) {
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] >= 'a' && s[i] <= 'z'){
+            s[i] = ((s[i] - 'a' + k) % 26) + 'a'; 
+        } else if (s[i] >= 'A' && s[i] <= 'Z'){
+            s[i] = ((s[i] - 'A' + k) % 26) + 'A';
+        }
+    }
+
+    return s;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string s;
+    getline(cin, s);
+
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string result = caesarCipher(s, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
